0c5936853fcbcecc1bb105ddb32444984eb555eb
Dominik Cubelic <dominik.cubelic@q-alliance.com>
2018-01-01 17:32:58 +0100

    Added search to people view

--- Debts/PeopleViewControllers/PeopleViewController.swift
+++ Debts/PeopleViewControllers/PeopleViewController.swift
@@ -6,13 +6,37 @@ class PeopleViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     let realm = try! Realm()
+    let searchController = UISearchController(searchResultsController: nil)
     
     var people: [Person] = []
+    var filteredPeople: [Person] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search People"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
         reloadPeople()
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+    
+    func filterPeople(for searchText: String) {
+        filteredPeople = people.filter({ (person) -> Bool in
+            return person.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
     }
     
     func reloadPeople() {
@@ -31,14 +55,25 @@ class PeopleViewController: UIViewController {
 
 extension PeopleViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering() {
+            return filteredPeople.count
+        }
+        
         return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .value1, reuseIdentifier: "DetailCell")
         
-        cell.textLabel?.text = people[indexPath.row].name
-        cell.detailTextLabel?.text = String(RealmHelper.getCost(for: people[indexPath.row]))
+        var person: Person
+        if isFiltering() {
+            person = filteredPeople[indexPath.row]
+        } else {
+            person = people[indexPath.row]
+        }
+        
+        cell.textLabel?.text = person.name
+        cell.detailTextLabel?.text = String(RealmHelper.getCost(for: person))
         
         return cell
     }
@@ -48,17 +83,39 @@ extension PeopleViewController: UITableViewDataSource, UITableViewDelegate {
         
         let vc = UIStoryboard(name: Constants.Storyboard.main, bundle: nil).instantiateViewController(withIdentifier: Constants.Storyboard.personDetailViewController) as! PersonDetailViewController
         
-        vc.person = people[indexPath.row]
+        var person: Person
+        if isFiltering() {
+            person = filteredPeople[indexPath.row]
+        } else {
+            person = people[indexPath.row]
+        }
+        
+        vc.person = person
         vc.hidesBottomBarWhenPushed = true
         
         navigationController?.pushViewController(vc, animated: true)
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        var person: Person
+        if isFiltering() {
+            person = filteredPeople[indexPath.row]
+        } else {
+            person = people[indexPath.row]
+        }
+        
         if editingStyle == .delete {
-            RealmHelper.removePerson(person: people[indexPath.row])
-            people.remove(at: indexPath.row)
+            if isFiltering() {
+                if let index = people.index(of: filteredPeople[indexPath.row]) {
+                    people.remove(at: index)
+                }
+                filteredPeople.remove(at: indexPath.row)
+            } else {
+                people.remove(at: indexPath.row)
+            }
+            
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            RealmHelper.removePerson(person: person)
         }
     }
 }
@@ -66,5 +123,13 @@ extension PeopleViewController: UITableViewDataSource, UITableViewDelegate {
 extension PeopleViewController: AddPersonViewControllerDelegate {
     func addPersonViewControllerDidAddPerson(_ vc: AddPersonViewController, person: Person) {
         reloadPeople()
+    }
+}
+
+extension PeopleViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filterPeople(for: searchText)
+        }
     }
 }
